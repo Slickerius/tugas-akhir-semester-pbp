4517a5be0ec86a04e8de41c785c0c54dff01d8cc
Adrian Hakim Utomo <adrianhakimutomo@gmail.com>
2021-12-29 00:11:37 +0700

    initial commit for infovaksin

--- Beranda/lib/info_vaksin.dart
+++ Beranda/lib/info_vaksin.dart
@@ -4,10 +4,55 @@ class InfoVaksinPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Center(
-          child: Text('Info Vaksin Page'),
-        )
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              BuildCard('Sinovac'),
+              BuildCard("Astrazeneca")
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              BuildCard('Sinopharm'),
+              BuildCard('Moderna')
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              BuildCard('Pfizer'),
+              BuildCard('Novavax')
+            ],
+          )
+        ],
+      )
     );
   }
-}
 
+  Card BuildCard(String namavaksin) {
+    return Card(
+              color: Color(0xffffb1b1),
+              elevation: 5,
+              child: SizedBox(
+                height: 120,
+                width: 120,
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    namavaksin,
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15
+                    ),
+                  ),
+                ),
+              ),
+            );
+  }
+}
